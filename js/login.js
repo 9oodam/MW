284b48818a6c7728c998adf3cd52b4230c7bdb67
ojhhh <winnel@naver.com>
2023-04-24 16:30:40 +0900

    login, signup accept merge

--- js/login.js
+++ js/login.js
@@ -1,84 +1,85 @@
 // ADMIN 계정 로컬스토리지 생성
 const adminAccount = {
-    name : "maple",
-    pw : "worrior",
-    lv : 2
-}
+  name: "maple",
+  pw: "worrior",
+  lv: 2,
+};
 localStorage.setItem("ADMIN", JSON.stringify(adminAccount));
-
 
 // USER 계정 로컬스토리지 받아와서 로그인
 let su = JSON.parse(localStorage.getItem("USER"));
 function login() {
-    // 회원가입 된 유저가 없을시
-    if(!su){
-        alert("No account exists");
-        return;
+  // 회원가입 된 유저가 없을시
+
+  // 입력한 아이디와 비밀번호를 가져옵니다.
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  if (!username) {
+    alert("Name is empty. Please fill in the blank.");
+    return;
+  } else if (!password) {
+    alert("Password is empty. Please fill in the blank.");
+    return;
+  } else if (!su) {
+    alert("No account exists");
+    return;
+  }
+
+  // admin 로그인
+  let admin = JSON.parse(localStorage.getItem("ADMIN"));
+  if (username == admin.name) {
+    console.log(admin.name);
+    console.log(admin.pw);
+    if (admin.name == username && admin.pw != password) {
+      alert("Wrong password. Please try again.");
+      return;
+    } else if (admin.name == username && admin.pw == password) {
+      alert("Login successful");
+      sessionStorage.setItem("ADMINLOGIN", JSON.stringify(admin));
+      location.reload(); // login 성공시 새로고침
+      return;
     }
+  }
 
-    // 입력한 아이디와 비밀번호를 가져옵니다.
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+  // user 로그인
+  //   console.log(su);
+  let suName = [];
+  su.forEach(function (value, index) {
+    suName.push(su[index].name);
+  });
+  //   console.log(suName);
+  //   console.log(username);
 
-    // admin 로그인
-    let admin = JSON.parse(localStorage.getItem("ADMIN"));
-    if(username == admin.name) {
-        console.log(admin.name);
-        console.log(admin.pw);
-        if(admin.name == username && admin.pw != password) {
-            alert("Wrong password. Please try again.");
-            return;
-        }else if(admin.name == username && admin.pw == password) {
-            alert('Login successful');
-            sessionStorage.setItem("ADMINLOGIN", JSON.stringify(admin));
-            location.reload(); // login 성공시 새로고침
-            return;
+  // 생성한 아이디와 비밀번호를 저장합니다.
+  su.forEach(function (value) {
+    // console.log(value.name);
+    // console.log(value.pw);
+    // 아이디와 비밀번호를 검증합니다.
+    if (username == value.name && password == value.pw && value.lv == 1) {
+      alert("Login successful");
+      let localuser = JSON.parse(localStorage.getItem("USER"));
+      localuser.forEach(function (value) {
+        if (value.name == username) {
+          // console.log(value);
+          let logininfo = {
+            name: value.name,
+            nickname: value.nickname,
+          };
+          sessionStorage.setItem("LOGIN", JSON.stringify(logininfo));
+          location.reload(); // login 성공시 새로고침
         }
+      });
+    } else if (
+      username == value.name &&
+      password == value.pw &&
+      value.lv == 0
+    ) {
+      alert("Waiting account acception...");
+    } else if (username != value.name && password == value.pw) {
+      alert("Wrong name. Please try again.");
+    } else if (username == value.name && password != value.pw) {
+      alert("Wrong password. Please try again.");
     }
-
-    // user 로그인
-    console.log(su);
-    let suName = [];
-    su.forEach(function(value, index) {
-        suName.push(su[index].name);
-    })
-    console.log(suName);
-    console.log(username);
-
-    if(!username) { 
-        alert("Name is empty. Please fill in the blank.");
-        return;
-
-    }else if(!password) { 
-        alert("Password is empty. Please fill in the blank.");
-        return;
-    }
-    // 생성한 아이디와 비밀번호를 저장합니다.
-    su.forEach(function(value){
-
-        // console.log(value.name);
-        // console.log(value.pw);
-        // 아이디와 비밀번호를 검증합니다.
-        if (username == value.name && password == value.pw && value.lv == 1) {
-            alert('Login successful');
-            let localuser = JSON.parse(localStorage.getItem("USER"));
-            localuser.forEach(function(value){
-                if(value.name == username){
-                    // console.log(value);
-                    let logininfo = {
-                        name: value.name,
-                        nickname: value.nickname
-                    }
-                    sessionStorage.setItem("LOGIN", JSON.stringify(logininfo));
-                    location.reload(); // login 성공시 새로고침
-                }
-            });
-        } else if(username == value.name && password == value.pw && value.lv == 0){
-            alert("Waiting account acception..."); 
-        } else if(username != value.name && password == value.pw) {
-            alert("Wrong name. Please try again.");
-        } else if(username == value.name && password != value.pw) {
-            alert("Wrong password. Please try again.");
-        }
-    })
+  });
 }
--- js/signup.js
+++ js/signup.js
@@ -1,12 +1,18 @@
 function signup() {
-
   let savedNickname = document.getElementById("nickname").value; //vaule 가져온다.
   let savedName = document.getElementById("name").value;
   let savedPassword = document.getElementById("signup_password").value;
 
-  // console.log(savedNickname);
-  // console.log(savedName);
-  // console.log(savedPassword);
+  if (!savedNickname) {
+    alert("Nickname is empty. Please fill in the blank.");
+    return;
+  } else if (!savedName) {
+    alert("Name is empty. Please fill in the blank.");
+    return;
+  } else if (!savedPassword) {
+    alert("Password is empty. Please fill in the blank.");
+    return;
+  }
 
   // create user list
   let userlist = {
@@ -19,21 +25,23 @@ function signup() {
   // localstroge에 USER가 없으면 if문 실행, 있으면 else문 실행
   if (!localStorage.getItem("USER")) {
     localStorage.setItem("USER", JSON.stringify([userlist]));
+    location.reload();
   } else {
+    let userList = JSON.parse(localStorage.getItem("USER"));
+    let nicknames = userList.map((user) => user.nickname);
+    let names = userList.map((user) => user.name);
+
+    if (nicknames.includes(savedNickname)) {
+      alert("이미 존재하는 닉네임입니다.");
+      return;
+    } else if (names.includes(savedName)) {
+      alert("이미 존재하는 이름입니다.");
+      return;
+    }
+
     let userpush = JSON.parse(localStorage.getItem("USER"));
     userpush.push(userlist);
     localStorage.setItem("USER", JSON.stringify(userpush));
     location.href = "./home.html"; // signup 후 home으로
-  } 
-  if(!savedNickname){
-    alert("Nickname is empty. Please fill in the blank.");
-    return;
-  } else if(!savedName) { 
-    alert("Name is empty. Please fill in the blank.");
-    return;
-  }else if(!savedPassword) { 
-    alert("Password is empty. Please fill in the blank.");
-    return;
   }
 }
-
