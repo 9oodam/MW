25a5ecad960cbc753b4414abbbb539685d434a92
ojhhh <winnel@naver.com>
2023-04-25 20:34:32 +0900

    login for문 오류 관련 find로 함수로 대체

--- js/login.js
+++ js/login.js
@@ -12,22 +12,18 @@ let su = JSON.parse(localStorage.getItem("USER"));
 let admin = JSON.parse(localStorage.getItem("ADMIN"));
 
 function login() {
-  // 회원가입 된 유저가 없을시
-
   // 입력한 아이디와 비밀번호를 가져옵니다.
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
 
   // admin 로그인
-  if (username == admin.name) {
-    // console.log(admin.name);
-    // console.log(admin.pw);
-    if (admin.name == username && admin.pw != password) {
+  if (username === adminAccount.name) {
+    if (password !== adminAccount.pw) {
       alert("Wrong password. Please try again.");
       return;
-    } else if (admin.name == username && admin.pw == password) {
+    } else {
       alert("Login successful");
-      sessionStorage.setItem("ADMINLOGIN", JSON.stringify(admin));
+      sessionStorage.setItem("ADMINLOGIN", JSON.stringify(adminAccount));
       location.reload(); // login 성공시 새로고침
       return;
     }
@@ -39,52 +35,37 @@ function login() {
   } else if (!password) {
     alert("Password is empty. Please fill in the blank.");
     return;
-  } else if (!su) {
+  }
+
+  // user 로그인
+  // USER 이름을 가진 localstorage가 있는 경우 왼쪽 없는 경우 빈배열을 생성
+  // USER 스토리지가 없는 경우를 대비
+  let users = JSON.parse(localStorage.getItem("USER")) || [];
+
+  // 입력한 아이디와 비밀번호를 검증합니다.
+  let user = users.find((u) => u.name === username);
+  console.log(user);
+
+  if (!user) {
     alert("No account exists");
     return;
   }
 
-  // user 로그인
-  //   console.log(su);
-  let suName = [];
-  su.forEach(function (value, index) {
-    suName.push(su[index].name);
-  });
-  //   console.log(suName);
-  //   console.log(username);
+  if (user.lv === 0) {
+    alert("Waiting account acceptance...");
+    return;
+  }
 
-  // 생성한 아이디와 비밀번호를 저장합니다.
-  su.forEach(function (value) {
-    // console.log(value.name);
-    // console.log(value.pw);
-    // 아이디와 비밀번호를 검증합니다.
-    if (username == value.name && password == value.pw && value.lv == 1) {
-      alert("Login successful");
-      let localuser = JSON.parse(localStorage.getItem("USER"));
-      localuser.forEach(function (value) {
-        if (value.name == username) {
-          // console.log(value);
-          let logininfo = {
-            name: value.name,
-            nickname: value.nickname,
-          };
-          sessionStorage.setItem("LOGIN", JSON.stringify(logininfo));
-          location.reload(); // login 성공시 새로고침
-        }
-      });
-    } else if (
-      username == value.name &&
-      password == value.pw &&
-      value.lv == 0
-    ) {
-      alert("Waiting account acception...");
-      return;
-    } else if (username != value.name && password == value.pw) {
-      alert("Wrong name. Please try again.");
-      return;
-    } else if (username == value.name && password != value.pw) {
-      alert("Wrong password. Please try again.");
-      return;
-    }
-  });
+  if (user.pw !== password) {
+    alert("Wrong password. Please try again.");
+    return;
+  }
+
+  alert("Login successful");
+  let logininfo = {
+    name: user.name,
+    nickname: user.nickname,
+  };
+  sessionStorage.setItem("LOGIN", JSON.stringify(logininfo));
+  location.reload(); // login 성공시 새로고침
 }
